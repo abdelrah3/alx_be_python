3a6cb9e1db53b935433303c49d8f0cbc38df3a15
abdelrah3 <ahmed.abdo.aa.1996@gmail.com>
2024-07-27 16:03:36 +0300

    arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,5 +9,7 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 print("Please enter a number other than 0")
-            else :
+            elif :
+                pass
+            else:
                 return num1 / num2
